c12dc46f5c6f217fb129dedb5b20d4bdf4a1bfe1
Yang Wang <BryanWang@Yangs-Air.fios-router.home>
2018-03-22 13:05:23 -0400

    initial commit

---
+++ src/components/Form.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import uuidv1 from 'uuid';
+import { addArticle } from '../actions/index';
+import PropTypes from 'prop-types';
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addArticle: article => dispatch(addArticle(article))
+  };
+}
+
+class ConnectedForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.id]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { title } = this.state;
+    const id = uuidv1();
+    this.props.addArticle({ title, id });
+    this.setState({ title: '' });
+  }
+
+  render() {
+    const { title } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            value={title}
+            onChange={this.handleChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-success btn-lg">
+          SAVE
+        </button>
+      </form>
+    )
+  }
+}
+
+const Form = connect(null, mapDispatchToProps) (ConnectedForm);
+
+ConnectedForm.propTypes = {
+  addArticle: PropTypes.func.isRequired
+};
+
+export default Form;
